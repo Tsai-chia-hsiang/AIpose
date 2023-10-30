424cabbecdf89fb8b92cce89a313b326dac75a53
Tsai-chia-hsiang <u08410098@ccu.edu.tw>
2023-10-30 19:11:51 +0800

    run sample

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from places365 import run
 import sys
 import os
+import cv2
+import numpy as np
 import os.path as osp
 
 place365rn50 = run.Place365_Pretrianed_ResNet50(
@@ -19,13 +21,43 @@ def walk_dir(root:os.PathLike)->list:
             u.append(os.path.join(r, f))             
     return u
 
-def main(cls=False, img = None): 
-    img = sys.argv[1] if cls else img 
-    p = predict(img)
-    print(p)
-    cluster_dir = osp.join("sample_skeleton", p['topic'][1])
-    suggestion_poses = walk_dir(cluster_dir)
-    print(suggestion_poses)
+def combine_pose_to_scene(scene:os.PathLike, pose:os.PathLike)->np.ndarray:
+    
+    def resize_pose_shape(poseshape:tuple, sceneshape:tuple)->tuple:
+        max_aix = np.argmax(poseshape) # 0 or 1
+        ratio = poseshape[1-max_aix]/poseshape[max_aix]
+        resize_aix_ratio = sceneshape[max_aix]/1.6
+        resize = [0,0]
+        resize[max_aix] = int(resize_aix_ratio)
+        resize[1-max_aix] = int(resize_aix_ratio*ratio)
+        resize.reverse()
+        return tuple(resize)
+
+    s = cv2.imread(scene).astype(np.float32)
+    spose = cv2.imread(pose)
+   
+    sshape = s.shape
+    spose = cv2.resize(spose,resize_pose_shape(spose.shape, sshape))
+    spose = spose.astype(np.float32)
+    w_left = sshape[1]//2 - spose.shape[1]//2
+
+    m = (spose <= 80.0).astype(np.float32)
+    posepart =s[s.shape[0]-spose.shape[0]:s.shape[0], w_left:w_left+spose.shape[1],:] 
+    
+    s[s.shape[0]-spose.shape[0]:s.shape[0], w_left:w_left+spose.shape[1],:] = posepart*m + spose
+    return np.clip(0, 255, s).astype(np.uint8)
+
+
+def run_smaple(sampleroot:os.PathLike): 
+
+    for smaplesecen in walk_dir(sampleroot):
+        p = predict(smaplesecen)
+        cluster_dir = osp.join("sample_skeleton", p['topic'][1])
+        suggestion_poses = walk_dir(cluster_dir)
+        d,f = osp.split(smaplesecen)
+        add_pose_img_path = os.path.join(osp.join(d, "result"), f)
+        combine = combine_pose_to_scene(smaplesecen, suggestion_poses[0])
+        cv2.imwrite(add_pose_img_path, combine)
 
 if __name__ == "__main__":
-    main(cls=True)
+    run_smaple(sys.argv[1])
